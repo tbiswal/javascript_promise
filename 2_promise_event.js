fd7ac2643f7b33aa6881136e6e6964d5ad941192
tanmayatechs@gmail.com <tanmayatechs@gmail.com>
2021-09-30 14:46:38 +0530

    Check immutable nature of promise

--- 2_promise_event.js
+++ 2_promise_event.js
@@ -1,8 +1,51 @@
 function foo(x) {
+	setTimeout(function() { 
+		console.log('Doing something here which will take a while')
+	}, 3000);
+
 	return new Promise( function(resolve, reject){
+		if( x > 30) {
+			setTimeout(function(){
+				console.log('Inside promise - True');
+				resolve("Promise is resolved!"); // fulfilled
+			}, 300);
+		} else {    
+			console.log('Inside promise - False');
+			reject('Promise is rejected!');  
+		}
 		
 	})
 }
 
-var p = foof(42);
+var p = foo(42);
 
+function bar(fooPromise) {
+	fooPromise.then(
+		function() {
+			let x = 10;
+			let y = 20;
+			console.log(`bar ${x + y}`);
+		},
+		function() {
+			console.log('oops, something went wrong in foo(...)');
+		}
+	)
+}
+
+bar(p);
+
+
+function baz(fooPromise) {
+	fooPromise.then(
+		function() {
+			let x = 10;
+			let y = 20;
+			console.log(`baz ${x - y}`);
+		},
+		function() {
+			console.log('oops, something went wrong in foo(...)');
+		}
+	)
+}
+
+baz(p);
